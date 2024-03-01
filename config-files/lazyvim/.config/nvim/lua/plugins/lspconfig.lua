888e7661e14313856bcbdc09bdc4dd83a72a9b36
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2024-03-01 15:37:03 -0300

    formatting provider

--- config-files/lazyvim/.config/nvim/lua/plugins/lspconfig.lua
+++ config-files/lazyvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -18,6 +18,13 @@ return {
       },
       setup = {
         eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
           vim.api.nvim_create_autocmd("BufWritePre", {
             callback = function(event)
               if not require("lazyvim.plugins.lsp.format").enabled() then
